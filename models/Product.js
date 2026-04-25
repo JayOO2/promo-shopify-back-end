54de3c67a0b91a02bc7b405a272420f89d8c52fb
๖ۣۜJay༻ 英雄 <137283300+JayOO2@users.noreply.github.com>
2026-04-26 04:53:42 +0530

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  imageUrl: { type: String },
+  rating: { type: Number, default: 0 },
+  ratedCount: { type: Number, default: 0 },
+  addedBy: { type: String, required: true }, // admin email
+  createdAt: { type: Date, default: Date.now },
+  price: {type: Number},
+  income: { type: Number, default: 0 },
+  reacts: {type: Number},
+  isLuckyOrderProduct: {type: String, default: "no"},
+  plan: { 
+  type: String, 
+  enum: [
+    "Starter", 
+    "Basic", 
+    "Beginner", 
+    "Advanced", 
+    "Professional", 
+    "Premium"
+  ], 
+  default: "Starter" 
+}
+});
+
+export default mongoose.model("Product", productSchema);
